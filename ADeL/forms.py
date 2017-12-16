36a10dd44214fd809b2637fc754edc544d2abb01
Mohamed Sanguiana Camara <nafinaconde0339@gmail.com>
2017-12-16 16:37:14 +0000

    Registration and login

--- ADeL/forms.py
+++ ADeL/forms.py
@@ -3,24 +3,24 @@ from .models import *
 from django.contrib.auth.models import User
 
 class userform(forms.ModelForm):
-	nom= forms.CharField(widget=forms.TextInput(
-		attrs={'class' :'form-control', 'placeholder':'Nom'}
-		), required=True, max_length=50)
-	prenom= forms.CharField(widget=forms.TextInput(
-		attrs={'class' :'form-control', 'placeholder':'Prénom'}
-		), required=True, max_length=50)
-	pseudo= forms.CharField(widget=forms.TextInput(
-		attrs={'class' :'form-control', 'placeholder':'Pseudo'}
+	username= forms.CharField(widget=forms.TextInput(
+		attrs={'class' :'form-control', 'placeholder':'Username'}
 		), required=True, max_length=50)
 	email= forms.CharField(widget=forms.EmailInput(
-		attrs={'class' :'form-control', 'placeholder':'Adresse Email'}
+		attrs={'class' :'form-control', 'placeholder':'Email'}
+		), required=True, max_length=50)
+	first_name= forms.CharField(widget=forms.TextInput(
+		attrs={'class' :'form-control', 'placeholder':'Prenom'}
+		), required=True, max_length=50)
+	last_name= forms.CharField(widget=forms.TextInput(
+		attrs={'class' :'form-control', 'placeholder':'Nom'}
 		), required=True, max_length=50)
 	password= forms.CharField(widget=forms.PasswordInput(
 		attrs={'class' :'form-control', 'placeholder':'Mot de passe'}
 		), required=True, max_length=50)
-	password1= forms.CharField(widget=forms.PasswordInput(
+	confirm_password= forms.CharField(widget=forms.PasswordInput(
 		attrs={'class' :'form-control', 'placeholder':'Confirmation'}
 		), required=True, max_length=50)
 	class Meta():
 		model=User
-		fields = ['nom', 'prenom', 'pseudo', 'email', 'password']
+		fields = ['username', 'email', 'first_name', 'last_name', 'password']
--- ADeL/views.py
+++ ADeL/views.py
@@ -3,22 +3,39 @@ from django.http import *
 from .forms import *
 from .models import *
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib import auth
+from django.core.exceptions import ObjectDoesNotExist
 def home(request):
 	return render(request,'pages/home.html')
 def connexion(request):
+	if request.method == "POST":
+		username =request.POST['user']
+		password =request.POST['pas']
+		try:
+			user= auth.authenticate(username=username,password=password)
+			if user is not None:
+				auth.login(request,user)
+				return render(request, 'pages/welcome.html')
+			else:
+				messages.errors(request,'username ou password incorrect')
+		except auth.ObjectNotExist:
+			print("user invalid")
+
 	return render(request,'pages/connexion.html')
 
 def register(request): 
 	if request.method == 'POST':
 		form1=userform(request.POST) 
 		if form1.is_valid():
-				nom = form1.cleaned_data['nom']
-				prenom = form1.cleaned_data['prenom']
-				pseudo = form1.cleaned_data['pseudo']    
-				email = form1.cleaned_data['email']  
+				username = form1.cleaned_data['username']
+				email = form1.cleaned_data['email']
+				first_name = form1.cleaned_data['first_name']    
+				last_name = form1.cleaned_data['last_name']  
 				password = form1.cleaned_data['password']  
-				password1 = form1.cleaned_data['password1'] 
-				User.objects.create_user(nom = nom, prenom = prenom, pseudo = pseudo, email = email, password = password) 
+				confirm_password = form1.cleaned_data['confirm_password'] 
+				User.objects.create_user(username = username, email = email, first_name = first_name, last_name = last_name, password = password) 
+				messages.success(request, 'Utilisateur enregistrer avec succès')
 				return HttpResponseRedirect('/register/')
 	else:
 		form1 = userform()
